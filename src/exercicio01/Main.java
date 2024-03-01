d37889a7c2ff056f157bbb236055c69269a5aeec
Heder David <hedermd6@gmail.com>
2024-03-01 15:30:57 -0300

    exercicio01 finalizado

--- src/exercicio01/Main.java
+++ src/exercicio01/Main.java
@@ -2,7 +2,7 @@ package exercicio01;
 
 public class Main {
     public static void main(String[] args) {
-        Date date = new Date(29, 02, 2024);
+        Date date = new Date(29, 05, 2024);
         System.out.println(date.showDate());
     }
 }

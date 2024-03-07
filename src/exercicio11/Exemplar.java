dad9b30d7ae2cf35409a0a25ece8748edd7f5a78
Heder David <hedermd6@gmail.com>
2024-03-07 17:27:00 -0300

    exercicio finalizado

--- src/exercicio11/Exemplar.java
+++ src/exercicio11/Exemplar.java
@@ -2,8 +2,8 @@ package exercicio11;
 
 public class Exemplar {
     private int numeroTombo;
-    private String tituloObra;
-    private String[] autoria;
+    private final String tituloObra;
+    private final String[] autoria;
     private Boolean habilitadoExemplarEmprestimo;
     private int quantidadeEmprestimosRegistrado = 0;
 
@@ -20,7 +20,6 @@ public class Exemplar {
             quantidadeEmprestimosRegistrado++;
         }
     }
-
     public void habilitarEmprestimo() {
         habilitadoExemplarEmprestimo = true;
     }
@@ -29,9 +28,24 @@ public class Exemplar {
         habilitadoExemplarEmprestimo = false;
     }
 
+    public int getNumeroTombo() {
+        return numeroTombo;
+    }
 
+    public String getTituloObra() {
+        return tituloObra;
+    }
 
+    public String[] getAutoria() {
+        return autoria;
+    }
 
+    public Boolean getHabilitadoExemplarEmprestimo() {
+        return habilitadoExemplarEmprestimo;
+    }
 
+    public int getQuantidadeEmprestimosRegistrado() {
+        return quantidadeEmprestimosRegistrado;
+    }
 
 }

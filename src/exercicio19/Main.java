b13de0c6be780849f8ceda09209cc950a1eed518
Heder David <hedermd6@gmail.com>
2024-03-10 12:10:34 -0300

    exercicio19 finalizado

--- src/exercicio19/Main.java
+++ src/exercicio19/Main.java
@@ -1,2 +1,16 @@
-package exercicio19;public class Main {
+package exercicio19;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[] {2, 4, 6, 8, 10, 12};
+
+        try {    // bloco de tratamento de exceção
+            for (int i = 0; i <= 12; i++) {
+                System.out.println(array[i]);   // acesso ao enésimo elemento do array
+            }
+        }        // bloco de captura de exceção
+        catch(ArrayIndexOutOfBoundsException excecao) {
+            excecao.printStackTrace();
+        }
+    }
 }

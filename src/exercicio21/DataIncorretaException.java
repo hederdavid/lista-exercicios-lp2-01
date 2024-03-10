f7d0d6a12d13566b2eb9e41959120193218858fa
Heder David <hedermd6@gmail.com>
2024-03-10 15:36:46 -0300

    exercicio21 finalizado

---
+++ src/exercicio21/DataIncorretaException.java
@@ -0,0 +1,8 @@
+package exercicio21;
+
+public class DataIncorretaException extends Exception {
+    public DataIncorretaException(String mensagem) {
+        super(mensagem);
+    }
+
+}
---
+++ src/exercicio21/Main.java
@@ -0,0 +1,62 @@
+package exercicio21;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int data = 0, mes = 0, ano = 0;
+        Date date = null;
+        boolean isNumeroCorreto = false;
+        do {
+            try {
+                do {
+                    try {
+                        System.out.print("Insira a data: ");
+                        data = scanner.nextInt();
+                        isNumeroCorreto = true;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Insira um número inteiro!");
+                        scanner.next();
+                    }
+                } while (!isNumeroCorreto);
+
+                isNumeroCorreto = false;
+
+                do {
+                    try {
+                        System.out.print("Insira o mês: ");
+                        mes = scanner.nextInt();
+                        isNumeroCorreto = true;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Insira um número inteiro!");
+                        scanner.next();
+                    }
+                } while (!isNumeroCorreto);
+
+                isNumeroCorreto = false;
+
+                do {
+                    try {
+                        System.out.print("Insira o ano: ");
+                        ano = scanner.nextInt();
+                        isNumeroCorreto = true;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Insira um número inteiro!");
+                        scanner.next();
+                    }
+                } while (!isNumeroCorreto);
+
+                isNumeroCorreto = false;
+                date = new Date(data, mes, ano);
+                isNumeroCorreto = true;
+            } catch (DataIncorretaException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!isNumeroCorreto);
+
+        System.out.println(date.showDate());
+        System.out.println(date.getDiasTerminoAno());
+    }
+}
